b67ec9b3223bb98a9a77b4f437b8daac4ac7db41
JackieCisneros <krieger.jacqueline@gmail.com>
2023-01-19 14:15:48 -0500

    commit tree 514450d218a7e649e91d9eb68cc42f3d99d4f3b3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(arrStrings, eventName){
+    let newArr = [];
+    for(let i = 0; i < arrStrings.length; i++){
+        newArr.push(`Thank you, ${arrStrings[i]}, for the wonderful ${eventName} gift!`);
+        console.log(newArr)
+    }
+    return newArr;
+}; 
+
+function countDown(integer){
+    while(integer >= 0){
+        console.log(integer);
+        integer--;
+    }
+}
+
+
